27cabd4500c1b73b4a32cdd4bfe9ddcd28869112
michalmlk <michal.melka1@gmail.com>
2023-05-19 16:29:33 +0200

    add css-variables

---
+++ tinder-clone-fe/src/templates/MainTemplate.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainTemplate.scss';
+
+const MainTemplate: React.FC = () => {
+	return (
+		<div className="wrapper">
+			<h1>Hello world</h1>
+		</div>
+	);
+};
+
+export default MainTemplate;
